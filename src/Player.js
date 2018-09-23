df3f85c31633d8745d1b35e6e63a2769627627b8
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-23 18:28:04 -0500

    showDetails working

--- src/Player.js
+++ src/Player.js
@@ -4,13 +4,30 @@ import PlayerDetails from './PlayerDetails.js';
 
 class Player extends Component {
 
-    
+    state = {
+        infoState: false
+    }
+
 
     render() {
+
+      const { infoState } = this.state
+
+      const displayDetails = () => {
+        this.setState({ 
+            infoState: !this.state.infoState })
+        }
+
       return (
         <div className="player">
           <PlayerInfo name={this.props.name} image={this.props.image} team_name={this.props.team_name} />
-          <PlayerDetails ind_points={this.props.ind_points} total_points={this.props.total_points} />
+          <button onClick={displayDetails}>Player Stats!</button>
+          {infoState ? (
+            <div>
+            <PlayerDetails ind_points={this.props.ind_points} total_points={this.props.total_points} />
+            </div>
+         ) : null}
+          
         </div>
       )
     }
